b339a165a86ace8763c8947dc8e752ad9e25315d
AlhyAbdellah <alhy.abdellah@gmail.com>
2025-05-03 21:54:23 +0200

    Fix safe_send arguments

--- config/config.py
+++ config/config.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv(dotenv_path="pages/.env")
 
 Email=os.getenv("LINKEDIN_EMAIL")
 Password=os.getenv("LINKEDIN_PASSWORD")
+print("EMAIL chargé depuis .env :", Email)
+print("passw chargé depuis .env :", Password)
